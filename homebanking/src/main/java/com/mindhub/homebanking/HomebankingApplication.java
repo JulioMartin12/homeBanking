e0723008516d791804e4b28c082f43b3deb75a88
faria <faria@DESKTOP-TLUUL68>
2023-08-04 17:18:21 -0300

    Finalizada la task1

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,13 +1,28 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.mindhub.homebanking.repositories.ClientRepository;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return  (args -> {
+
+			Client client = new Client("Julio", "Martin", "Rl_yoo@yahoo.com.ar");
+			clientRepository.save(client);
+		});
 	}
 
 }
