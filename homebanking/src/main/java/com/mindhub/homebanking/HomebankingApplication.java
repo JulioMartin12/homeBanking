2e5241ea46283fbef4283244696c291d0235588c
Julio Martin <faria@DESKTOP-TLUUL68>
2023-09-11 19:17:46 -0300

    comentados los datos hardcodeados

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -41,7 +41,7 @@ public class HomebankingApplication {
 			clientRepository.save(admin);
 */
 
-			Client melba = new Client("Melba", "Morel", "melba@mindHub.com", passwordEncoder.encode("12345"), RoleType.CLIENT);
+		/*	Client melba = new Client("Melba", "Morel", "melba@mindHub.com", passwordEncoder.encode("12345"), RoleType.CLIENT);
 			clientRepository.save(melba);
 
 			Account account = new Account("VIN001", LocalDate.now(),5000);
@@ -149,7 +149,7 @@ public class HomebankingApplication {
 			Client Josesito = new Client("Josesito", "Lopez", "Jose@yahoo.com.ar",passwordEncoder.encode("12345"),RoleType.CLIENT);
 			clientRepository.save(martin);
 
-		});
+*/		});
 	}
 
 }
